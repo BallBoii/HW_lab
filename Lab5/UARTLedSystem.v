cc8c894c5615d385cd0ee0dd17f93a8b85b1f882
Thanakrit_B <thanakirt47@gmail.com>
2025-02-23 20:03:06 +0700

    Add Lab5

---
+++ Lab5/UARTLedSystem.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Create Date: 01/26/2025 11:53:46 PM
+// Design Name: UARTLedSystem
+// Module Name: UARTLedSystem
+// Project Name: UARTLedSystem
+// Target Devices: Basys3
+// Tool Versions: 2023.2
+// Description: The top level module for the UARTLedSystem project.
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module UARTLedSystem (
+    input  wire        Clk,
+    input  wire        Reset,
+    input  wire        SentUartData,
+    input  wire [ 7:0] Sw,
+    output wire        UARTTx,
+    input  wire        UARTRx,
+    output wire [15:0] Led
+);
+  // Add your code here
+
+  // End or your code
+endmodule
